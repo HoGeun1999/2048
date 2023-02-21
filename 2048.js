38cb05df67387508a41ff3df9f5415fa638c8348
Ho <cgh100039@gmail.com>
2023-02-21 21:20:05 +0900

    make structure

--- 2048.js
+++ 2048.js
@@ -1,28 +1,63 @@
-var table = document.getElementById('table')
-var new_game_button = document.getElementById('new_game')
-td_list = []
+const table = document.getElementById('table')
+const new_game_button = document.getElementById('new_game')
+const td_list = []
+const td_data_list = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+]
+window.addEventListener("keydown", key_in )
 
+function key_in(event){
+    if (event.key === 'ArrowDown'){
+        if(td_data_list[1][0] == 0){
+            td_data_list[1][0] = td_data_list[0][0]
+            td_data_list[0][0] = 0 
+        }
+        if(td_data_list[2][0] == 0){
+            td_data_list[2][0] = td_data_list[1][0] 
+            td_data_list[1][0] = 0
+        }
+        if(td_data_list[3][0] == 0){
+            td_data_list[3][0] = td_data_list[2][0] 
+            td_data_list[2][0] = 0
+        }
+    }
+
+    updateTable()
+}
 function set_table(){
 
     for(i = 0; i < 4; i++){
-        var tr = document.createElement('tr')
+        const tr = document.createElement('tr')
         tr.id = i+1
         table.appendChild(tr)
         for(j = 0; j < 4; j++){
-            var td = document.createElement('td')
+            const td = document.createElement('td')
             td.id = i*4+j+1
             tr.appendChild(td)
             td_list.push(td)
         }
     }
 
-
 }
 
 function new_game(){
     set_table()
+    td_data_list[0][0] = 2
     td_list[0].textContent = '2'
-    Math.random()
 }
+
+function updateTable(){
+    for(let i =0; i < 4; i++){
+        for(let j =0; j < 4; j++){
+            if(td_data_list[i][j] != 0){
+                td_list[i*4+j].textContent = td_data_list[i][j]
+            }
+        }
+    }
+}
+
 
 new_game()
